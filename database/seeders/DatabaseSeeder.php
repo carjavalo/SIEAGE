39dc3d335b1ab6835f8ee12a19cc58de7eaaaff2
KeviinDCV <Pandatrap1999@gmail.com>
2026-09-24 08:07:15 -0500

    refactor: simplificar la consulta de barrios en el controlador de inscripción

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,11 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            DatosInicialesSeeder::class,
-            // Después de los datos iniciales, para completar los barrios que ya
-            // traen (y sus ids) en vez de duplicarlos.
-            BarriosCaliSeeder::class,
-        ]);
+        $this->call(DatosInicialesSeeder::class);
     }
 }
